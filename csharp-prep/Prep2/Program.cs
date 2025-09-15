fa21869ad404362c24d2cb3f8b3df1c79ac53ea6
Jonathan-Tullis <jonathantullis171@gmail.com>
2025-09-15 11:29:09 -0600

    Completed Prep 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,9 +1,45 @@
 using System;
+using System.Runtime.CompilerServices;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("Enter your grade percentage");
+
+        int grade = int.Parse(Console.ReadLine());
+
+        string letter = "";
+
+        if (grade >= 90)
+        {
+            letter = "A";
+        }
+        else if (grade >= 80 && grade <= 89)
+        {
+            letter = "B";
+        }
+        else if (grade >= 70 && grade <= 79)
+        {
+            letter = "C";
+        }
+        else if (grade >= 60 && grade <= 69)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        if (grade >= 70)
+        {
+            Console.WriteLine($"you passed with a {letter}, at {grade} percent!");
+        }
+        else
+        {
+            Console.WriteLine($"You failed with a {letter}, at {grade} percent.");
+        }
+
     }
 }
