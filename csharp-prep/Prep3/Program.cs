5492fc7ec9e227df4b1ed6ff8703005ddf9b7383
Jonathan-Tullis <jonathantullis171@gmail.com>
2025-09-20 12:49:13 -0600

    Finished Prep 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,45 +1,57 @@
 using System;
 using System.Runtime.CompilerServices;
+using System.Collections.Generic;
 
 // This is a number guessing game where the user has to guess a randomly generated number between 0 and whatever you want.
 class Program
 {
     static void Main(string[] args)
     {
-        
-
-
-
-
-        Console.Write("Guess a number between 1 and 100: ");
-
-        Random randomGenerator = new Random();
-        int number = randomGenerator.Next(1, 101);
-
-
-        bool hasGuessedNumber = false;
-        while (!hasGuessedNumber)
+        bool playAgain = true;
+        while (playAgain)
         {
-            string input = Console.ReadLine();
-            int userInput = int.Parse(input);
+            Console.WriteLine("Guess a number between 1 and 100");
 
+            Random randomGenerator = new Random();
+            int number = randomGenerator.Next(1, 101);
 
-            if (userInput > number)
+            int numberOfGuesses = 1;
+            bool hasGuessedNumber = false;
+            while (!hasGuessedNumber)
             {
-                Console.WriteLine("Guess Lower");
+                Console.Write("What is your guess?? ");
+                string input = Console.ReadLine();
+                int userInput = int.Parse(input);
+
+                if (userInput > number)
+                {
+                    Console.WriteLine("Guess Lower");
+                    numberOfGuesses++;
+                }
+                else if (userInput < number)
+                {
+                    Console.WriteLine("Guess Higher");
+                    numberOfGuesses++;
+                }
+                else
+                {
+                    Console.WriteLine("You guessed it!");
+                    hasGuessedNumber = true;
+                }
+
             }
-            else if (userInput < number)
+
+            Console.WriteLine($"You completed the game in {numberOfGuesses} guesses!");
+
+            Console.Write("Do you want to play again? ");
+            string response = Console.ReadLine();
+
+            if (response.ToLower() != "yes")
             {
-                Console.WriteLine("Guess Higher");
-            }
-            else
-            {
-                Console.WriteLine("Correct!");
-                hasGuessedNumber = true;
+                playAgain = false;
             }
 
         }
 
-        Console.WriteLine("You have completed the game!");   
     }
 }
