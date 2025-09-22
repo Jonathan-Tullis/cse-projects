cdc6439ecd9868e9aa01dd1f086c3b72af734eea
Jonathan-Tullis <jonathantullis171@gmail.com>
2025-09-22 12:59:17 -0600

    Finished Abstraction Activity

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,26 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._company = "Idaho National Laboratory";
+        job1._jobTitle = "Software Engineer";
+        job1._startYear = 2020;
+        job1._endYear = 2024;
+
+
+
+        Job job2 = new Job();
+        job2._company = "Google";
+        job2._jobTitle = "Machine Learning Engineer";
+        job2._startYear = 2024;
+        job2._endYear = 2025;
+
+        Resume resume = new Resume();
+        resume._personsName = "Jonathan Tullis";
+
+        resume._jobs.Add(job1);
+        resume._jobs.Add(job2);
+
+        resume.DisplayJobDetails();
     }
 }
---
+++ prepare/Learning02/Resume.cs
@@ -0,0 +1,17 @@
+public class Resume
+{
+    public string _personsName = "";
+    public List<Job> _jobs = new List<Job>();
+
+    public void DisplayJobDetails()
+    {
+        Console.WriteLine($"Name: {_personsName}");
+        Console.WriteLine("Jobs:");
+
+        foreach (Job job in _jobs)
+        {
+            job.DisplayJobDetails();
+        }
+    }
+
+}
